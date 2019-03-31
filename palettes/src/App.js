ab20ffab4e4bf82d0018c66142ee9a82225e8b77
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-03-31 11:59:22 -0400

    Make swatch data stateful via useState

--- palettes/src/App.js
+++ palettes/src/App.js
@@ -3,11 +3,13 @@ import React, { useState, useEffect, useContext } from 'react';
 import Palette from './Palette';
 
 const App = () => {
-  const swatches = [
+  const initialSwatches = [
     [255, 0, 0],
     [0, 255, 0],
     [0, 0, 255],
   ];
+
+  const [swatches, setSwatches] = useState(initialSwatches);
 
   return (
     <Palette swatches={ swatches } />
